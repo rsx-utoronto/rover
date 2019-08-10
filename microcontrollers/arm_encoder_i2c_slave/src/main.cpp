755f40c91e76849fad5006040c888d0506331fb8
hschier <hudsonschier@gmail.com>
2019-08-09 19:40:20 -0600

    Simplified code, each board now supports 3 sensors at all times

--- microcontrollers/arm_encoder_i2c_slave/src/main.cpp
+++ microcontrollers/arm_encoder_i2c_slave/src/main.cpp
@@ -3,27 +3,16 @@
 
 #define slave_address 1
 
-#if slave_address == 1
-    #define total_no_of_sensors 2
-#elif slave_address == 2
-    #define total_no_of_sensors 2
-#elif slave_address == 3
-    #define total_no_of_sensors 2
-#endif
-
-const int outputA_pins[3] = {4, 9, 11};
-const int outputB_pins[3] = {5, 10, 12};
-int Sensor_values[total_no_of_sensors] = { 0 };
-int aStates[total_no_of_sensors] = { 0 };
-int bStates[total_no_of_sensors] = { 0 };
-int bLastStates[total_no_of_sensors] = { 0 };
-int aLastStates[total_no_of_sensors] = { 0 };
+const int encoder_a_pins[3] = {4, 9, 11};
+const int encoder_b_pins[3] = {5, 10, 12};
+int Sensor_values[3] = { 0 };
+int aStates[3] = { 0 };
+int bStates[3] = { 0 };
+int bLastStates[3] = { 0 };
+int aLastStates[3] = { 0 };
 byte Sensor_value_bytes[2] = { 7 };
 
 int currentSensor = 0;
-// encoder 1: 0
-// encoder 2: 1
-// encoder 3: 2
 
 void receiveEvent(int numBytes);
 void requestEvent();
@@ -35,32 +24,31 @@ void setup() {
     Serial.begin(115200);
 
     // Setting all pinModes and Sensor states
-    for (int i = 0; i < total_no_of_sensors; i++) {
-        pinMode(outputA_pins[i], INPUT);
-        pinMode(outputB_pins[i], INPUT);
-        aLastStates[i] = digitalRead(outputA_pins[i]);
-        bLastStates[i] = digitalRead(outputB_pins[i]);
+    for (int i = 0; i < 3; i++) {
+        pinMode(encoder_a_pins[i], INPUT);
+        pinMode(encoder_b_pins[i], INPUT);
+        aLastStates[i] = digitalRead(encoder_a_pins[i]);
+        bLastStates[i] = digitalRead(encoder_b_pins[i]);
     }
 }
 
 void loop() {
-    
-    // Go through n number of sensors and update them all
-    for (int i = 0; i < total_no_of_sensors; i++) {
-        aStates[i] = digitalRead(outputA_pins[i]);  // Reads the "current" state of the outputA[i]
-        bStates[i] = digitalRead(outputB_pins[i]);
-        if(aStates[i] != aLastStates[i]){
-            if(aStates[i]) {
+    // Update each sensor's state
+    for (int i = 0; i < 3; i++) {
+        aStates[i] = digitalRead(encoder_a_pins[i]);  // Reads the "current" state
+        bStates[i] = digitalRead(encoder_b_pins[i]);
+        // compare to previous state, increment or decrement
+        if (aStates[i] != aLastStates[i]) {
+            if (aStates[i]) {
                 bStates[i] ? Sensor_values[i]-- : Sensor_values[i]++;
             } else {
                 bStates[i] ? Sensor_values[i]++ : Sensor_values[i]--;
             }
         }
 
-        if(bLastStates[i] != bStates[i]){
+        if(bLastStates[i] != bStates[i]) {
             if(bStates[i]) {
                 aStates[i] ? Sensor_values[i]++ : Sensor_values[i]--;
-                
             } else {
                 aStates[i] ? Sensor_values[i]-- : Sensor_values[i]++;
             }
@@ -68,14 +56,14 @@ void loop() {
         bLastStates[i] = bStates[i];
         aLastStates[i] = aStates[i];
     }
-    Serial.println(Sensor_values[0]);
-    //delay(); // might not be needed maybe
+    // Serial.println(Sensor_values[0]);
+    // delay(); // might not be needed maybe
 }
 
 void receiveEvent(int numBytes) {
     // Set currentSensor so the desired sensor is reported
     currentSensor = Wire.read();
-    Serial.println(currentSensor);
+    // Serial.println(currentSensor);
 }
 
 void requestEvent() {
