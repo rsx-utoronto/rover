600014b693ffd1094f8e4b370caa77e36e2f4057
ellenxwchen <44852580+ellenxwchen@users.noreply.github.com>
2019-11-30 14:38:47 -0500

    Update map.js

--- src/MapGui/map.js
+++ src/MapGui/map.js
@@ -4,3 +4,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?acce
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
 }).addTo(mymap);
+
+mymap.on('click', addMarker);
+
+function addMarker(e){
+  
+  //add marker
+  circleMarker = new  L.circle(e.latlng, 5, {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 1.0
+            }).addTo(mymap);
+}
