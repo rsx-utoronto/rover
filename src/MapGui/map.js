65fa085fd0d0cb240ef15f8d0c18360d958f73a8
ellenxwchen <44852580+ellenxwchen@users.noreply.github.com>
2019-11-30 13:39:11 -0500

    Update map.js

--- src/MapGui/map.js
+++ src/MapGui/map.js
@@ -1,8 +1,6 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([43.6532, -79.3832], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZWxsZW54d2NoZW4iLCJhIjoiY2szbHZlc2RiMDJiejNmcWVteGx3aHNjcSJ9.KTf1PHkOPk_pXswRLLu8YA', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
