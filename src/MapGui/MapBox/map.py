352c8d1aa6431b67722d5e05efa3da7576100f39
Shichao Jiang <xtornado47@gmail.com>
2020-02-22 16:48:01 -0500

    - Added full offline capabality (currently Regina) - Must run server.py before running map.py - Updated index.html to support offline capability

---
+++ src/MapGui/MapBox/map.py
@@ -0,0 +1,9 @@
+#Need to install pywebview before running
+#install with "pip install pywebview"
+
+#run server.py before running this file
+
+import webview
+
+webview.create_window('RSX Map Software', 'http://localhost:8080/index.html')
+webview.start()
--- src/MapGui/MapBox/test.py
+++
@@ -1,7 +0,0 @@
-#Need to install pywebview before running
-#install with "pip install pywebview"
-
-import webview
-
-webview.create_window('RSX Map Software', 'index.html')
-webview.start()
