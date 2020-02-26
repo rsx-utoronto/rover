c03f53dce42e076ea9e17a5daabfa2f9e6a6a2b5
Hudson Schier <hudsonschier@gmail.com>
2020-02-26 13:17:35 -0500

    Put all ESC objects in an array

--- microcontrollers/drive_board/src/main.cpp
+++ microcontrollers/drive_board/src/main.cpp
@@ -10,36 +10,39 @@ void setup() {
 }
 
 void loop() {
-	static ESC ESC1(2340.0, 24, 25, 8, 22, 23, B0001001);
-	static ESC ESC2(2340.0, 26, 27, 9, 28, 29, B0001101);
-	static ESC ESC3(2340.0, 33, 32, 7, 31, 30, B0001000);
-	static ESC ESC4(2340.0, 35, 34, 4, 37, 36, B0001100);
-	static ESC ESC5(2340.0, 39, 38, 5, 41, 40, B0001110);
-	static ESC ESC6(2340.0, 45, 44, 6, 43, 42, B1001100);
+	// Declare these objects here because ROS does not like to have
+	// global objects for some reason
+	static ESC Drivers[6] = {
+		ESC(2340.0, 24, 25, 8, 22, 23, B0001001),
+		ESC(2340.0, 26, 27, 9, 28, 29, B0001101),
+		ESC(2340.0, 33, 32, 7, 31, 30, B0001000),
+		ESC(2340.0, 35, 34, 4, 37, 36, B0001100),
+		ESC(2340.0, 39, 38, 5, 41, 40, B0001110),
+		ESC(2340.0, 45, 44, 6, 43, 42, B1001100)
+	};
+
+	// Only run this code once
 	static bool first_run = true;
 	if(first_run) {
-		ESC1.set_enable(false);
-		ESC2.set_enable(false);
-		ESC3.set_enable(false);
-		ESC4.set_enable(false);
-		ESC5.set_enable(false);
-		ESC6.set_enable(false);
-		delay(1000);
-		ESC1.set_enable(true);
-		ESC2.set_enable(true);
-		ESC3.set_enable(true);
-		ESC4.set_enable(true);
-		ESC5.set_enable(true);
-		ESC6.set_enable(true);
-		delay(1000);
+		reset_driver_faults(Drivers);
 		first_run = false;
 	}
+}
 
-	float a = -200.0;
-	ESC1.set_vel(a);
-	ESC2.set_vel(a);
-	ESC3.set_vel(a);
-	ESC4.set_vel(a);
-	ESC5.set_vel(a);
-	ESC6.set_vel(a);
+void reset_driver_faults(ESC Drivers[6]){
+	// Call this if the motors fault
+	for(int i = 0; i < 6; i++) {
+		Drivers[i].set_enable(false);
+	}
+	delay(100);
+	for(int i = 0; i < 6; i++) {
+		Drivers[i].set_enable(true);
+	}
+	delay(100);
+}
+
+void set_all_vel(float vel, ESC Drivers[6]) {
+	for(int i = 0; i < 6; i++) {
+		Drivers[i].set_vel(vel);
+	}
 }
---
+++ microcontrollers/drive_board/src/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h
+#define main_h
+
+void reset_driver_faults(ESC[6]);
+void set_all_vel(float, ESC[6]);
+
+#endif
