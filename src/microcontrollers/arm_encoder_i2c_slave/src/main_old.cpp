6c53acf2173bf7c725ed200d415dc45793910489
hkseo <seorichard@hotmail.com>
2022-07-09 13:11:17 -0400

    adding modified arm code

--- src/microcontrollers/arm_encoder_i2c_slave/src/main_old.cpp
+++ src/microcontrollers/arm_encoder_i2c_slave/src/main_old.cpp
@@ -5,16 +5,13 @@
 
 const int encoder_a_pins[3] = {4, 9, 11};
 const int encoder_b_pins[3] = {5, 10, 12};
+int Sensor_values[3] = { 0 };
+int aStates[3] = { 0 };
+int bStates[3] = { 0 };
+int bLastStates[3] = { 0 };
+int aLastStates[3] = { 0 };
 
-volatile int bLastStates[3] = { 0 };
-volatile int aLastStates[3] = { 0 };
-volatile int aStates[3] = { 0 };
-volatile int bStates[3] = { 0 };
-int count_values[3] = {0};
-
-void loop(){
-  }
-
+int currentSensor = 0;
 
 void receiveEvent(int numBytes);
 void requestEvent();
@@ -25,20 +22,17 @@ void setup() {
     Wire.onRequest(requestEvent);
     Serial.begin(115200);
     pinMode(LED_BUILTIN, INPUT);
- 
+
     // Setting all pinModes and Sensor states
     for (int i = 0; i < 3; i++) {
         pinMode(encoder_a_pins[i], INPUT);
         pinMode(encoder_b_pins[i], INPUT);
-        attachInterrupt(digitalPinToInterrupt(encoder_a_pins[i]), isr,CHANGE);
-        attachInterrupt(digitalPinToInterrupt(encoder_b_pins[i]), isr,CHANGE);
+        aLastStates[i] = digitalRead(encoder_a_pins[i]);
+        bLastStates[i] = digitalRead(encoder_b_pins[i]);
     }
 }
 
-
-
-
-void isr(){
+void loop() {
     // Update each sensor's state
     for (int i = 0; i < 3; i++) {
         aStates[i] = digitalRead(encoder_a_pins[i]);  // Reads the "current" state
@@ -46,27 +40,27 @@ void isr(){
         // compare to previous state, increment or decrement
         if (aStates[i] != aLastStates[i]) {
             if (aStates[i]) {
-                bStates[i] ? count_values[i]-- : count_values[i]++;
+                bStates[i] ? Sensor_values[i]-- : Sensor_values[i]++;
             } else {
-                bStates[i] ? count_values[i]++ : count_values[i]--;
+                bStates[i] ? Sensor_values[i]++ : Sensor_values[i]--;
             }
         }
 
         if(bLastStates[i] != bStates[i]) {
             if(bStates[i]) {
-                aStates[i] ? count_values[i]++ : count_values[i]--;
+                aStates[i] ? Sensor_values[i]++ : Sensor_values[i]--;
             } else {
-                aStates[i] ? count_values[i]-- : count_values[i]++;
+                aStates[i] ? Sensor_values[i]-- : Sensor_values[i]++;
             }
         }
         bLastStates[i] = bStates[i];
         aLastStates[i] = aStates[i];
-         Serial.print(i);
-         Serial.print(" = ");
-         Serial.print(count_values[i]);
-         Serial.print(", ");
+        // Serial.print(i);
+        // Serial.print(" = ");
+        // Serial.print(Sensor_values[i]);
+        // Serial.print(", ");
     }
-     Serial.println();
+    // Serial.println();
     // delay(); // might not be needed maybe
 }
 
@@ -78,8 +72,8 @@ void requestEvent() { // Report back the value of the sensor requested
     digitalWrite(LED_BUILTIN, 1);
     byte out_bytes[6];
     for (int i = 0; i < 3; i++) {
-        out_bytes[2 * i] = (count_values[i] >> 8) & 0xFF;
-        out_bytes[2 * i + 1] = count_values[i] & 0xFF;
+        out_bytes[2 * i] = (Sensor_values[i] >> 8) & 0xFF;
+        out_bytes[2 * i + 1] = Sensor_values[i] & 0xFF;
     }
     Wire.write(out_bytes, 6);
     digitalWrite(LED_BUILTIN, 0);
